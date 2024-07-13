c2716ae505be5e7d00bcd37874e68e3a9fbd7393
Vikram Segta <vikramsegta43@gmail.com>
2024-07-14 03:16:23 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={() => router.push("/tabs")}>to tabs</button>
+    </main>
+  );
+}
